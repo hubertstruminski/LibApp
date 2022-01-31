4c3cec8dbc5a6f0561f746ec3256978255a71be8
Hubert <hubert.struminski@gmail.com>
2022-01-31 19:37:53 +0100

    Added server-side validation for CustomerForm

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -10,15 +10,17 @@ namespace LibApp.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please provide custome's name")]
         [StringLength(255)]
         public string Name { get; set; }
         public bool HasNewsletterSubscribed { get; set; }
         public MembershipType MembershipType { get; set; }
         [Display(Name = "Membership Type")]
+        [Required(ErrorMessage = "Please provide membership type")]
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of birth")]
+        [Min18YearsIfMember]
         public DateTime? Birthdate { get; set; }
 
         public Customer()
