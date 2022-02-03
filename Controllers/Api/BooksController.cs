019e6e982807f866eba5b22c05de59d5c7baaae7
Hubert <hubert.struminski@gmail.com>
2022-02-03 21:14:26 +0100

    Added API controller, DTO, AutoMapper profile for Book model, added typeahead to Book input field in New Rental view

---
+++ Controllers/Api/BooksController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using LibApp.Data;
+using LibApp.Dtos;
+using LibApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibApp.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public BooksController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        // GET api/books
+        [HttpGet]
+        public IEnumerable<BookDto> GetBooks(string query = null)
+        {
+            var booksQuery = _context.Books.Where(b => b.NumberAvailable > 0);
+
+            if(!String.IsNullOrWhiteSpace(query))
+            {
+                booksQuery = booksQuery.Where(b => b.Name.Contains(query));
+            }
+
+            return booksQuery.ToList().Select(_mapper.Map<Book, BookDto>);
+        }
+    }
+}
